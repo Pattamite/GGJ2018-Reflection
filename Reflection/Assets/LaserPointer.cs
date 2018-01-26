804455ba99993243907c26de2507678ffb2451cb
Pattnatt <jittapat007@gmail.com>
2018-01-26 20:23:05 +0700

    Laser Hit Dectection

--- Reflection/Assets/LaserPointer.cs
+++ Reflection/Assets/LaserPointer.cs
@@ -16,21 +16,29 @@ public class LaserPointer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 firePosition = firePositionObject.transform.position;
+        Vector2 firePosition = firePositionObject.transform.position;
         float fireAngle = transform.eulerAngles.z;
         float xFireVector = Mathf.Cos(fireAngle / 180f * Mathf.PI);
         float yFireVector = Mathf.Sin(fireAngle / 180f * Mathf.PI);
+        Vector2 fireDirection = new Vector2(xFireVector, yFireVector);
 
-        drawLaser(firePosition, xFireVector, yFireVector);
+        drawLaser(firePosition, fireDirection);
+
+
+        if (Physics2D.Raycast(firePosition, fireDirection)) {
+            print("Hit");
+        }
+        else {
+            print("Not Hit");
+        }
     }
 
-    private void drawLaser(Vector3 firePosition, float xFireVector, float yFireVector) {
+    private void drawLaser(Vector2 firePosition, Vector2 fireDirection) {
         Vector3[] positions = new Vector3[2];
-        print(xFireVector.ToString() + " " + yFireVector.ToString());
         positions[0] = firePosition;
-        positions[1] = new Vector3(firePosition.x + (xFireVector * lineLength),
-                                   firePosition.y + (yFireVector * lineLength),
-                                   firePosition.z);
+        positions[1] = new Vector3(firePosition.x + (fireDirection.x * lineLength),
+                                   firePosition.y + (fireDirection.y * lineLength),
+                                   firePositionObject.transform.position.z);
         lineRenderer.positionCount = positions.Length; //change later
         lineRenderer.SetPositions(positions);
 
