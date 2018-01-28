6144cd28cbc5703ae16e71fbc23578045b2d7418
Pattnatt <jittapat007@gmail.com>
2018-01-28 07:01:10 +0700

    Paste Enemy code to Enemy_boss code

--- Reflection/Assets/Scripts/Enemy_Boss.cs
+++ Reflection/Assets/Scripts/Enemy_Boss.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 
 public class Enemy_Boss : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public int maxHP = 10;
+    public int attackDamage = 1;
+    private int currentHP;
+    private GameController gameController;
+
+    void Start () {
+        currentHP = maxHP;
+        gameController = GameObject.FindObjectOfType<GameController>();
+
+        gameController.enemyCount++;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    public void GetHit (int damage) {
+        currentHP -= damage;
+
+        if (currentHP <= 0) {
+            Kill();
+        }
+    }
+
+    public bool IsAlive () {
+        return currentHP > 0;
+    }
+
+    public void Kill () {
+        Destroy(gameObject);
+        gameController.enemyCount--;
+    }
 }
